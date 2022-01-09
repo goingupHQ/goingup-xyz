50845557c3ad6af56629dbbfc95ad81e29b58637
markibanez <mark.ibanez@gmail.com>
2022-01-09 09:04:52 +0800

    Connect wallet code

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const providerOptions = {
+        /* See Provider Options Section */
+    }
+
+    const web3Modal = new Web3Modal({
+        network: "mainnet", // optional
+        cacheProvider: true, // optional
+        providerOptions // required
+    })
+
+    const connectClick = async () => {
+        const instance = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(instance)
+        const signer = provider.getSigner()
+
+        console.log(instance, provider, signer)
+        alert('hello')
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>goingup.xyz</h1>
+                <button onClick={connectClick}>Connect your wallet</button>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
