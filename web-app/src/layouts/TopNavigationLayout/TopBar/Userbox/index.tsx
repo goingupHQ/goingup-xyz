051f90d66ca0ad1cb217c874e3234f80fd4c6468
markibanez <mark.ibanez@gmail.com>
2022-01-25 14:29:40 +0800

    Update index.tsx

--- web-app/src/layouts/TopNavigationLayout/TopBar/Userbox/index.tsx
+++ web-app/src/layouts/TopNavigationLayout/TopBar/Userbox/index.tsx
@@ -1,19 +1,20 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import { WalletProvider } from 'src/contexts/WalletContext';
 import { useAuth } from 'src/hooks/useAuth';
 import { useRouter } from 'next/router';
 
 import {
-  Avatar,
-  Box,
-  Button,
-  Divider,
-  alpha,
-  List,
-  ListItem,
-  ListItemText,
-  Popover,
-  styled,
-  Typography
+    Avatar,
+    Box,
+    Button,
+    Divider,
+    alpha,
+    List,
+    ListItem,
+    ListItemText,
+    Popover,
+    styled,
+    Typography
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import InboxTwoToneIcon from '@mui/icons-material/InboxTwoTone';
@@ -23,7 +24,7 @@ import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone';
 import AccountTreeTwoToneIcon from '@mui/icons-material/AccountTreeTwoTone';
 
 const UserBoxButton = styled(Button)(
-  ({ theme }) => `
+    ({ theme }) => `
     padding: ${theme.spacing(0, 1)};
     color: ${theme.colors.alpha.trueWhite[50]};
     background-color: ${theme.colors.alpha.white[10]};
@@ -58,33 +59,33 @@ const UserBoxButton = styled(Button)(
 );
 
 const MenuUserBox = styled(Box)(
-  ({ theme }) => `
+    ({ theme }) => `
     background: ${theme.colors.alpha.black[5]};
     padding: ${theme.spacing(2)};
 `
 );
 
 const UserBoxText = styled(Box)(
-  ({ theme }) => `
+    ({ theme }) => `
     text-align: left;
     padding-left: ${theme.spacing(1)};
 `
 );
 
 const UserBoxDescription = styled(Typography)(
-  ({ theme }) => `
+    ({ theme }) => `
         color: ${theme.palette.secondary.light};
 `
 );
 
 const UserBoxDescriptionMain = styled(Typography)(
-  ({ theme }) => `
+    ({ theme }) => `
         color: ${theme.colors.alpha.trueWhite[50]};
 `
 );
 
 const UserBoxLabel = styled(Typography)(
-  ({ theme }) => `
+    ({ theme }) => `
     font-weight: ${theme.typography.fontWeightBold};
     color: ${theme.palette.secondary.main};
     display: block;
@@ -92,7 +93,7 @@ const UserBoxLabel = styled(Typography)(
 );
 
 const UserBoxLabelMain = styled(Typography)(
-  ({ theme }) => `
+    ({ theme }) => `
     font-weight: ${theme.typography.fontWeightBold};
     display: block;
     color: ${theme.colors.alpha.trueWhite[100]};
@@ -100,149 +101,164 @@ const UserBoxLabelMain = styled(Typography)(
 );
 
 function Userbox() {
-  const { t }: { t: any } = useTranslation();
-  const { logout } = useAuth();
-  const router = useRouter();
+    // const walletProvider = useRef(null);
+    // useEffect(() => {
+    //     console.log(walletProvider.current)
+    // }, []);
+    const { t }: { t: any } = useTranslation();
+    const { logout } = useAuth();
+    const router = useRouter();
 
-  const user = {
-    avatar: '/static/images/avatars/1.jpg',
-    name: 'Rachael Simons',
-    jobtitle: 'Lead Developer'
-  };
+    const user = {
+        // avatar: '/static/images/avatars/1.jpg',
+        name: 'Rachael Simons',
+        jobtitle: 'Lead Developer'
+    };
 
-  const ref = useRef<any>(null);
-  const [isOpen, setOpen] = useState<boolean>(false);
+    const ref = useRef<any>(null);
+    const [isOpen, setOpen] = useState<boolean>(false);
 
-  const handleOpen = (): void => {
-    setOpen(true);
-  };
+    const handleOpen = (): void => {
+        setOpen(true);
+    };
 
-  const handleClose = (): void => {
-    setOpen(false);
-  };
+    const handleClose = (): void => {
+        setOpen(false);
+    };
 
-  const handleLogout = async (): Promise<void> => {
-    try {
-      handleClose();
-      await logout();
-      router.push('/');
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const handleLogout = async (): Promise<void> => {
+        try {
+            handleClose();
+            await logout();
+            router.push('/');
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  return (
-    <>
-      <UserBoxButton fullWidth color="secondary" ref={ref} onClick={handleOpen}>
-        <Avatar variant="rounded" alt={user.name} src={user.avatar} />
-        <Box
-          display="flex"
-          flex={1}
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Box
-            component="span"
-            sx={{
-              display: { xs: 'none', md: 'inline-block' }
-            }}
-          >
-            <UserBoxText>
-              <UserBoxLabelMain variant="body1">{user.name}</UserBoxLabelMain>
-              <UserBoxDescriptionMain variant="body2">
-                {user.jobtitle}
-              </UserBoxDescriptionMain>
-            </UserBoxText>
-          </Box>
-          <UnfoldMoreTwoToneIcon
-            fontSize="small"
-            sx={{
-              ml: 1
-            }}
-          />
-        </Box>
-      </UserBoxButton>
-      <Popover
-        disableScrollLock
-        anchorEl={ref.current}
-        onClose={handleClose}
-        open={isOpen}
-        anchorOrigin={{
-          vertical: 'center',
-          horizontal: 'center'
-        }}
-        transformOrigin={{
-          vertical: 'center',
-          horizontal: 'center'
-        }}
-      >
-        <MenuUserBox
-          sx={{
-            minWidth: 210
-          }}
-          display="flex"
-        >
-          <Avatar variant="rounded" alt={user.name} src={user.avatar} />
-          <UserBoxText>
-            <UserBoxLabel variant="body1">{user.name}</UserBoxLabel>
-            <UserBoxDescription variant="body2">
-              {user.jobtitle}
-            </UserBoxDescription>
-          </UserBoxText>
-        </MenuUserBox>
-        <Divider
-          sx={{
-            mb: 0
-          }}
-        />
-        <List
-          sx={{
-            p: 1
-          }}
-          component="nav"
-        >
-          <ListItem
-            onClick={() => {
-              handleClose();
-            }}
-            button
-          >
-            <AccountBoxTwoToneIcon fontSize="small" />
-            <ListItemText primary={t('Profile')} />
-          </ListItem>
-          <ListItem
-            onClick={() => {
-              handleClose();
-            }}
-            button
-          >
-            <InboxTwoToneIcon fontSize="small" />
-            <ListItemText primary={t('Inbox')} />
-          </ListItem>
-          <ListItem
-            onClick={() => {
-              handleClose();
-            }}
-            button
-          >
-            <AccountTreeTwoToneIcon fontSize="small" />
-            <ListItemText primary={t('Projects')} />
-          </ListItem>
-        </List>
-        <Divider />
-        <Box m={1}>
-          <Button color="primary" fullWidth onClick={handleLogout}>
-            <LockOpenTwoToneIcon
-              sx={{
-                mr: 1
-              }}
-            />
-            {t('Sign out')}
-          </Button>
-        </Box>
-      </Popover>
-    </>
-  );
+    return (
+        <WalletProvider>
+            <UserBoxButton
+                fullWidth
+                color="secondary"
+                ref={ref}
+                onClick={handleOpen}
+            >
+                <Avatar variant="rounded" alt={user.name} src={user.avatar} />
+                <Box
+                    display="flex"
+                    flex={1}
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Box
+                        component="span"
+                        sx={{
+                            display: { xs: 'none', md: 'inline-block' }
+                        }}
+                    >
+                        <UserBoxText>
+                            <UserBoxLabelMain variant="body1">
+                                {user.name}
+                            </UserBoxLabelMain>
+                            <UserBoxDescriptionMain variant="body2">
+                                {user.jobtitle}
+                            </UserBoxDescriptionMain>
+                        </UserBoxText>
+                    </Box>
+                    <UnfoldMoreTwoToneIcon
+                        fontSize="small"
+                        sx={{
+                            ml: 1
+                        }}
+                    />
+                </Box>
+            </UserBoxButton>
+            <Popover
+                disableScrollLock
+                anchorEl={ref.current}
+                onClose={handleClose}
+                open={isOpen}
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center'
+                }}
+                transformOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center'
+                }}
+            >
+                <MenuUserBox
+                    sx={{
+                        minWidth: 210
+                    }}
+                    display="flex"
+                >
+                    <Avatar
+                        variant="rounded"
+                        alt={user.name}
+                        src={user.avatar}
+                    />
+                    <UserBoxText>
+                        <UserBoxLabel variant="body1">{user.name}</UserBoxLabel>
+                        <UserBoxDescription variant="body2">
+                            {user.jobtitle}
+                        </UserBoxDescription>
+                    </UserBoxText>
+                </MenuUserBox>
+                <Divider
+                    sx={{
+                        mb: 0
+                    }}
+                />
+                <List
+                    sx={{
+                        p: 1
+                    }}
+                    component="nav"
+                >
+                    <ListItem
+                        onClick={() => {
+                            handleClose();
+                        }}
+                        button
+                    >
+                        <AccountBoxTwoToneIcon fontSize="small" />
+                        <ListItemText primary={t('Profile')} />
+                    </ListItem>
+                    <ListItem
+                        onClick={() => {
+                            handleClose();
+                        }}
+                        button
+                    >
+                        <InboxTwoToneIcon fontSize="small" />
+                        <ListItemText primary={t('Inbox')} />
+                    </ListItem>
+                    <ListItem
+                        onClick={() => {
+                            handleClose();
+                        }}
+                        button
+                    >
+                        <AccountTreeTwoToneIcon fontSize="small" />
+                        <ListItemText primary={t('Projects')} />
+                    </ListItem>
+                </List>
+                <Divider />
+                <Box m={1}>
+                    <Button color="primary" fullWidth onClick={handleLogout}>
+                        <LockOpenTwoToneIcon
+                            sx={{
+                                mr: 1
+                            }}
+                        />
+                        {t('Sign out')}
+                    </Button>
+                </Box>
+            </Popover>
+        </WalletProvider>
+    );
 }
 
 export default Userbox;
