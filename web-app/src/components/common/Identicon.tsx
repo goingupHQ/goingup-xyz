88b348a6dcfdb7a430f45d627f0af0d51e5d4fd6
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-26 09:41:17 +0800

    Create Identicon.tsx

---
+++ web-app/src/components/common/Identicon.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+import Jazzicon from '@metamask/jazzicon';
+
+interface IProps {
+  address: string;
+  size?: number;
+}
+
+export default function Identicon({ address, size }: IProps) {
+  const ref = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (address && ref.current) {
+      ref.current.innerHTML = '';
+      ref.current.appendChild(
+        Jazzicon(size || 36, parseInt(address.slice(2, 10), 16))
+      );
+    }
+  }, [address]);
+
+  return <div className="rounded-2xl pt-2" ref={ref as any} />;
+}
