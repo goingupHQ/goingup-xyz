8daed9a5d51e450d689635c0ece5d3b5e7de9aa6
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-29 14:13:10 +0800

    Create AppTypes.ts

---
+++ web-app/src/contexts/AppTypes.ts
@@ -0,0 +1,14 @@
+export type Availability = {
+    id: number;
+    text: string;
+}
+
+export type Occupation = {
+    id: number;
+    text: string;
+}
+
+export type UserGoal = {
+    id: number;
+    text: string;
+}
