fd0ee79f8b4414696e01b3189206065dd5ad0555
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-29 14:14:14 +0800

    Refactored to use app context data

--- web-app/pages/create-account/personal-info.tsx
+++ web-app/pages/create-account/personal-info.tsx
@@ -8,15 +8,15 @@ const fieldStyle = {
 
 function PersonalInfo() {
     const appContext = useContext(AppContext);
-    const { availability } = appContext;
+    const { availability, occupations } = appContext;
     return (
         <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
             <TextField label="First Name" variant="outlined" required sx={fieldStyle} />
             <TextField label="Last Name" variant="outlined" required sx={fieldStyle} />
-            <TextField label="Email Name" variant="outlined" required sx={fieldStyle} />
+            <TextField label="Email Address" variant="outlined" required sx={fieldStyle} />
             <TextField label="Discord" variant="outlined" sx={fieldStyle} />
 
-            <FormControl sx={fieldStyle}>
+            <FormControl sx={fieldStyle} required>
                 <InputLabel id="occupation-select-label">Occupation</InputLabel>
                 <Select
                     labelId="occupation-select-label"
@@ -24,22 +24,13 @@ function PersonalInfo() {
                     label="Occupation"
                     // onChange={handleChange}
                 >
-                    <MenuItem value="Artist">Artist</MenuItem>
-                    <MenuItem value="Developer">Developer</MenuItem>
-                    <MenuItem value="Athlete">Athlete</MenuItem>
-                    <MenuItem value="Marketing & Community">Marketing &amp; Community</MenuItem>
-                    <MenuItem value="Investor">Investor</MenuItem>
-                    <MenuItem value="Accountant">Accountant</MenuItem>
-                    <MenuItem value="Engineer">Engineer</MenuItem>
-                    <MenuItem value="Actor">Actor</MenuItem>
-                    <MenuItem value="Video Producer">Video Producer</MenuItem>
-                    <MenuItem value="Writer">Writer</MenuItem>
-                    <MenuItem value="Other">Other</MenuItem>
-
+                    {occupations.map(o => {return (
+                        <MenuItem value={o.id}>{o.text}</MenuItem>
+                    )})}
                 </Select>
             </FormControl>
 
-            <FormControl sx={fieldStyle}>
+            <FormControl sx={fieldStyle} required>
                 <InputLabel id="availability-select-label">Availability</InputLabel>
                 <Select
                     labelId="availability-select-label"
