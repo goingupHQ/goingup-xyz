ccf07f404abddf37944eb8e28ddb6b2b5817e4a7
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-29 23:33:48 +0800

    Refactored create account form to separate component

--- web-app/pages/create-account/index.tsx
+++ web-app/pages/create-account/index.tsx
@@ -1,24 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
+import { WalletContext } from 'src/contexts/WalletContext';
 import TopNavigationLayout from 'src/layouts/TopNavigationLayout';
 import {
-    Box,
-    Button,
     Grid,
     Card,
     CardHeader,
     CardContent,
     Typography,
-    Avatar,
-    Step,
-    Stepper,
-    StepLabel,
-    TextField,
-    styled
+    styled,
+    Button
 } from '@mui/material';
-import PersonalInfo from './personal-info';
-import ProjectGoals from './project-goals';
-import InviteFriends from './invite-friends';
+import CreateAccountForm from './create-account-form';
+import { error } from 'console';
 
 const CardContentWrapper = styled(CardContent)(
     () => `
@@ -26,54 +20,8 @@ const CardContentWrapper = styled(CardContent)(
   `
 );
 
-const steps = ['Personal Information', 'Project Goals', 'Invite Friends'];
-
 function CreateAccount() {
-    const [activeStep, setActiveStep] = useState(0);
-    const [skipped, setSkipped] = useState(new Set<number>());
-
-    const isStepOptional = (step: number) => {
-        // return step === 1;
-        return false;
-    };
-
-    const isStepSkipped = (step: number) => {
-        return skipped.has(step);
-    };
-
-    const handleNext = () => {
-        let newSkipped = skipped;
-        if (isStepSkipped(activeStep)) {
-            newSkipped = new Set(newSkipped.values());
-            newSkipped.delete(activeStep);
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleSkip = () => {
-        if (!isStepOptional(activeStep)) {
-            // You probably want to guard against something like this,
-            // it should never occur unless someone's actively trying to break something.
-            throw new Error("You can't skip a step that isn't optional.");
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped((prevSkipped) => {
-            const newSkipped = new Set(prevSkipped.values());
-            newSkipped.add(activeStep);
-            return newSkipped;
-        });
-    };
-
-    const handleReset = () => {
-        setActiveStep(0);
-    };
+    const wallet = useContext(WalletContext);
 
     return (
         <>
@@ -108,10 +56,17 @@ function CreateAccount() {
                                     <Typography variant="h1">
                                         Create Account
                                     </Typography>
-                                    <Typography variant="subtitle1">
-                                        Fill in the fields below to sign up for
-                                        an account
-                                    </Typography>
+                                    {wallet.address !== null && (
+                                        <Typography variant="subtitle1">
+                                            Fill in the fields below to sign up for an account
+                                        </Typography>
+                                    )}
+
+                                    {wallet.address == null && (
+                                        <Typography variant="subtitle1">
+                                            Connect your wallet first
+                                        </Typography>
+                                    )}
                                 </>
                             }
                         />
@@ -121,94 +76,18 @@ function CreateAccount() {
                                 pt: 0
                             }}
                         >
-                            <Stepper activeStep={activeStep}>
-                                {steps.map((label, index) => {
-                                    const stepProps: { completed?: boolean } =
-                                        {};
-                                    const labelProps: {
-                                        optional?: React.ReactNode;
-                                    } = {};
-                                    if (isStepOptional(index)) {
-                                        labelProps.optional = (
-                                            <Typography variant="caption">
-                                                Optional
-                                            </Typography>
-                                        );
-                                    }
-                                    if (isStepSkipped(index)) {
-                                        stepProps.completed = false;
-                                    }
-                                    return (
-                                        <Step key={label} {...stepProps}>
-                                            <StepLabel {...labelProps}>
-                                                {label}
-                                            </StepLabel>
-                                        </Step>
-                                    );
-                                })}
-                            </Stepper>
-                            {activeStep === steps.length ? (
-                                <React.Fragment>
-                                    <Typography sx={{ mt: 2, mb: 1 }}>
-                                        All steps completed - you&apos;re
-                                        finished
-                                    </Typography>
-                                    <Box
-                                        sx={{
-                                            display: 'flex',
-                                            flexDirection: 'row',
-                                            pt: 2
-                                        }}
-                                    >
-                                        <Box sx={{ flex: '1 1 auto' }} />
-                                        <Button onClick={handleReset}>
-                                            Reset
-                                        </Button>
-                                    </Box>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    {/* <Typography sx={{ mt: 2, mb: 1 }}>
-                                        Step {activeStep + 1}
-                                    </Typography> */}
+                            {wallet.address !== null && <CreateAccountForm />}
 
-                                    {activeStep === 0 && <PersonalInfo />}
-                                    {activeStep === 1 && <ProjectGoals />}
-                                    {activeStep === 2 && <InviteFriends />}
-
-                                    <Box
-                                        sx={{
-                                            display: 'flex',
-                                            flexDirection: 'row',
-                                            pt: 2
-                                        }}
-                                    >
-                                        <Button
-                                            color="inherit"
-                                            disabled={activeStep === 0}
-                                            onClick={handleBack}
-                                            sx={{ mr: 1 }}
-                                        >
-                                            Back
-                                        </Button>
-                                        <Box sx={{ flex: '1 1 auto' }} />
-                                        {isStepOptional(activeStep) && (
-                                            <Button
-                                                color="inherit"
-                                                onClick={handleSkip}
-                                                sx={{ mr: 1 }}
-                                            >
-                                                Skip
-                                            </Button>
-                                        )}
-                                        <Button onClick={handleNext}>
-                                            {activeStep === steps.length - 1
-                                                ? 'Finish'
-                                                : 'Next'}
-                                        </Button>
-                                    </Box>
-                                </React.Fragment>
-                            )}
+                            {wallet.address === null &&
+                            <>
+                                <Typography variant="h3">
+                                    A wallet address is required to create an account
+                                </Typography>
+                                <Button variant="contained" sx={{ mt: 3 }} onClick={wallet.connect}>
+                                    Click here to connect a wallet
+                                </Button>
+                            </>
+                            }
                         </CardContentWrapper>
                     </Card>
                 </Grid>
