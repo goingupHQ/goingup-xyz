1aaf7bad7f8f2f48264edb1e6ebbaeb24268c85f
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-30 14:45:12 +0800

    Fixed cors not working

--- api/server.js
+++ api/server.js
@@ -6,9 +6,15 @@ import { ethers } from 'ethers';
 const app = express();
 
 if (process.env.ENVIRONMENT === 'production') {
-    app.use(cors({
-        origin: ['goingup-xyz.vercel.app']
-    }));
+    const ALLOWED_ORIGINS = ['goingup-xyz.vercel.app']
+    app.use((req, res, next) => {
+        let origin = req.headers.origin;
+        let allowed = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
+        res.header("Access-Control-Allow-Origin", allowed);
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        next();
+    })
 }
 
 if (process.env.ENVIRONMENT === 'dev') {
