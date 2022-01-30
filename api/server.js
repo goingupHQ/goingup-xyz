c8fddcff402aaeaafd1f2a5131d1f44b1259cb82
Mark Ibanez <mark.ibanez@gmail.com>
2022-01-30 12:47:30 +0800

    Added /get-signer-address endpoint

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 import {} from 'dotenv/config';
 import express from 'express';
+import { ethers } from 'ethers';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/get-signer-address', (req, res) => {
+    const { message, signature } = req.query;
+    const recoveredAddress = ethers.utils.verifyMessage(message, signature);
+    res.send({ recoveredAddress });
 });
 
 app.listen(process.env.PORT, () => {
