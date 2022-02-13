72238ce39c4dca079e1cbd1c248cdbd00d3ea87a
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-13 11:36:42 +0800

    Update create-account.ts

--- web-app/pages/api/create-account.ts
+++ web-app/pages/api/create-account.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { ethers } from 'ethers';
 import { dbClient } from './_get-db-client';
 import { validateSignature } from './_validate-signature';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
     if (req.method !== 'POST') {
         res.status(405).send({ message: 'Only POST requests allowed' });
         return;
