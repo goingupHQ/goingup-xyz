3a522ef8f22a7089a29705d465ea0ccc963668f2
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-13 20:51:03 +0800

    Create update-account.ts

---
+++ web-app/pages/api/update-account.ts
@@ -0,0 +1,24 @@
+import { ethers } from 'ethers';
+import { dbClient } from './_get-db-client';
+import { validateSignature } from './_validate-signature';
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' });
+        return;
+    }
+    const body = req.body;
+    const isSignatureValid = validateSignature(body.address, 'update-account', body.signature);
+
+    if (isSignatureValid) {
+        await dbClient.connect();
+        const db = dbClient.db('main');
+        const accounts = db.collection('accounts');
+        const account = body.account;
+
+        const result = await accounts.updateOne({ address: body.address }, { $set: account });
+        res.status(200).send('account-updated');
+    } else {
+        res.status(401).send('invalid-signature');
+    }
+}
