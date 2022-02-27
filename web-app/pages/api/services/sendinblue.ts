b9b83970d08911658fc7d1d2bb3bef05b8c48ea7
Mark Ibanez <mark.ibanez@gmail.com>
2022-02-27 08:30:41 +0800

    Set a few parameters to optional

--- web-app/pages/api/services/sendinblue.ts
+++ web-app/pages/api/services/sendinblue.ts
@@ -4,20 +4,22 @@ var client = sdk.ApiClient.instance;
 var apiKey = client.authentications['api-key'];
 apiKey.apiKey = process.env.SIB_KEY;
 
-export const send = async (sendToName: string, sendToEmail: string, subject: string, textContent: string, htmlContent: string) => {
+export const sendEmail = async (sendToName: string, sendToEmail: string, subject: string, textContent: string, htmlContent: string) => {
     var apiInstance = new sdk.TransactionalEmailsApi();
-    var sendSmtpEmail = new sdk.SendSmtpEmail();
-    sendSmtpEmail = {
+    var email = new sdk.SendSmtpEmail();
+    email = {
         sender: { name: 'GoingUP', email: 'app@goingup.xyz' },
         to: [{
             email: sendToEmail,
-            name: sendToName
         }],
-        subject,
-        textContent,
-        htmlContent
+        subject
     };
-    apiInstance.sendTransacEmail(sendSmtpEmail).then(
+
+    if (sendToName) email.to[0].name = sendToName;
+    if (textContent) email.textContent = textContent;
+    if (htmlContent) email.htmlContent = htmlContent;
+
+    apiInstance.sendTransacEmail(email).then(
         function (data) {
             console.log('API called successfully. Returned data: ' + data);
         },
