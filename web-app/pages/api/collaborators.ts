a3342d19b9986989c9b93a1bfce6e6ba79b95a4b
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-16 18:03:14 +0800

    Update collaborators.ts

--- web-app/pages/api/collaborators.ts
+++ web-app/pages/api/collaborators.ts
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const db = await getDb();
     const accounts = await db.collection('accounts');
 
-    const query = {};
+    const query: any = {};
     if (openToArray.length) query.openTo = { $in: openToArray };
 
     const queryResults = await accounts.aggregate([
