c312de68330d90bfc5d6695ffc493bc9a0c34158
Mark Ibanez <mark.ibanez@gmail.com>
2022-05-24 13:21:19 +0800

    Update _document.tsx

--- web-app/pages/_document.tsx
+++ web-app/pages/_document.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import createEmotionCache from 'src/createEmotionCache';
-import Script from 'next/script';
 
 export default class MyDocument extends Document {
     render() {
