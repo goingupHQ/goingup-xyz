ec89d13dda3ef42e6592607bf0797e006d759fcd
Mark Ibanez <mark.ibanez@gmail.com>
2022-07-19 09:36:24 +0800

    Theme changes

--- web-app-2.0/components/AwardTokenButton.js
+++ web-app-2.0/components/AwardTokenButton.js
@@ -1,23 +1,23 @@
 import { Button, Typography } from '@mui/material';
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function AwardTokenButton() {
-  const [active, setActive] = useState(true)
-  function handleClick() {
-    setActive(!active)
-  }
-  return (
-    <Button
-      variant='contained'
-      color={active ? "primary" : "activePrimary"}
-      sx={{
-        ':hover': {
-          backgroundColor: 'hoverPrimary.main',
-        },
-      }}
-      onClick={handleClick}
-    >
-      <Typography color={'#0F151C'}>Award Token</Typography>
-    </Button>
-  );
+    const [active, setActive] = useState(true);
+    function handleClick() {
+        setActive(!active);
+    }
+    return (
+        <Button
+            variant="contained"
+            color="primary"
+            sx={{
+                ':hover': {
+                    backgroundColor: 'hoverPrimary.main',
+                },
+            }}
+            onClick={handleClick}
+        >
+            Award Token
+        </Button>
+    );
 }
--- web-app-2.0/components/layout.js
+++ web-app-2.0/components/layout.js
@@ -3,100 +3,114 @@ import { useContext, useEffect, useMemo } from 'react';
 import { AppContext } from '../contexts/app-context';
 
 export default function Layout({ children }) {
-  const app = useContext(AppContext);
+    const app = useContext(AppContext);
 
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: app.mode,
-          primary: {
-            main: '#F4CE00',
-          },
-          hoverPrimary: {
-            main: '#FFE555',
-          },
-          activePrimary: {
-            main: '#FFF4B8',
-          },
-          secondary: {
-            main: '#7A55FF',
-          },
-          text1: {
-            main: '#FFFFFF',
-          },
-          text1: {
-            main: '#4D5F72',
-          },
-          background1: {
-            main: '#0F151C',
-          },
-          background2: {
-            main: '#19222C',
-          },
-          backgroundLight: {
-            main: '#FFFFFF'
-          },
-          hoverFollow: {
-            main: '#5537C3',
-            text: '#F4CE00',
-          },
-          activeFollow: {
-            main: '#3AB795',
-            text: '#FFFFFF',
-          },
-          hoverViewAllProjects: {
-            main: '#F4CE00',
-          },
-          activeViewAllProjects: {
-            main: '#D2B200',
-          }
-        },
-        typography: {
-          fontFamily: 'Questrial',
-          h1: {
-            fontSize: '32px',
-            fontWeight: 'bold',
-          },
-          h2: {
-            fontSize: '24px',
-            fontWeight: 'bold',
-          },
-          h3: {
-            fontSize: '18px',
-            fontWeight: 'bold',
-          },
-          sh1: {
-            fontSize: '16px',
-            fontWeight: '500',
-            color: '#4D5F72',
-          },
-          sh2: {
-            fontSize: '14px',
-            fontWeight: '500',
-            color: '#4D5F72',
-          },
-          sh3: {
-            fontSize: '12px',
-            fontWeight: '500',
-            color: '#4D5F72',
-          },
-          p: {
-            fontSize: '16px',
-            fontWeight: 'medium',
-          },
-        },
-        spacing: [0, 10, 15, 20, 30, 60, 80],
-      }),
-    [app.mode]
-  );
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: app.mode,
+                    primary: {
+                        main: '#F4CE00',
+                    },
+                    hoverPrimary: {
+                        main: '#FFE555',
+                    },
+                    activePrimary: {
+                        main: '#FFF4B8',
+                    },
+                    secondary: {
+                        main: '#7A55FF',
+                    },
+                    text1: {
+                        main: '#FFFFFF',
+                    },
+                    text1: {
+                        main: '#4D5F72',
+                    },
+                    background1: {
+                        main: '#0F151C',
+                    },
+                    background2: {
+                        main: '#19222C',
+                    },
+                    backgroundLight: {
+                        main: '#FFFFFF',
+                    },
+                    hoverFollow: {
+                        main: '#5537C3',
+                        text: '#F4CE00',
+                    },
+                    activeFollow: {
+                        main: '#3AB795',
+                        text: '#FFFFFF',
+                    },
+                    hoverViewAllProjects: {
+                        main: '#F4CE00',
+                    },
+                    activeViewAllProjects: {
+                        main: '#D2B200',
+                    },
+                },
+                typography: {
+                    fontFamily: 'Questrial',
+                    h1: {
+                        fontSize: '32px',
+                        fontWeight: 'bold',
+                    },
+                    h2: {
+                        fontSize: '24px',
+                        fontWeight: 'bold',
+                    },
+                    h3: {
+                        fontSize: '18px',
+                        fontWeight: 'bold',
+                    },
+                    sh1: {
+                        fontSize: '16px',
+                        fontWeight: '500',
+                        color: '#4D5F72',
+                    },
+                    sh2: {
+                        fontSize: '14px',
+                        fontWeight: '500',
+                        color: '#4D5F72',
+                    },
+                    sh3: {
+                        fontSize: '12px',
+                        fontWeight: '500',
+                        color: '#4D5F72',
+                    },
+                    p: {
+                        fontSize: '16px',
+                        fontWeight: 'medium',
+                    },
+                    button: {
+                        textTransform: 'capitalize',
+                        fontWeight: 'bold',
+                    },
+                },
+                shape: {
+                    borderRadius: '12px',
+                },
+                components: {
+                    MuiButtonBase: {
+                        defaultProps: {
+                            style: {
+                                padding: '10px 40px'
+                            }
+                        }
+                    }
+                },
+                spacing: [0, 10, 15, 20, 30, 60, 80],
+            }),
+        [app.mode]
+    );
 
-  useEffect(() => {
-    document.body.style.backgroundColor =
-      app.mode === 'dark'
-        ? theme.palette.background.default
-        : theme.palette.background.default;
-  }, [app.mode]);
+    useEffect(() => {
+        document.body.style.backgroundColor =
+            app.mode === 'dark' ? theme.palette.background.default : theme.palette.background.default;
+    }, [app.mode]);
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
