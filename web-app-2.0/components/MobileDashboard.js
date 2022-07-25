70383cd10955f2dda721d2a0efef812174ab066f
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2022-07-24 20:56:45 -0700

    need to target menu icon for mobile dash

--- web-app-2.0/components/MobileDashboard.js
+++ web-app-2.0/components/MobileDashboard.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
 import DashboardIcon from "./icons/DashboardIcon";
 import ProjectsIcon from "./icons/ProjectsIcon";
@@ -7,95 +8,92 @@ import SearchBox from "./SearchBox";
 import MessageIcon from "./icons/MessageIcon";
 import GlobeIcon from "./icons/GlobeIcon";
 import SettingsIcon from "./icons/SettingsIcon";
+import MenuIcon from "./icons/MenuIcon";
 
 export default function MobileDashboard() {
+    const [showMobileDashboard, setShowMobileDashboard] = useState(false);
+    const onClick = () => setShowMobileDashboard(true);
+
     return (
-        <Stack
-            spacing={3}
-            alignItems="flex-start"
-            sx={{
-                marginX: "25px",
-                marginY: "30px",
-                display: { sm: "none" },
-            }}
-        >
-            <Box width='100%' marginBottom={1}>
-                <SearchBox />
-            </Box>
-            <Button
-                sx={{
-                    color: "#F4CE00",
-                    ":hover": {
-                        backgroundColor: "hoverPrimary.main",
-                    },
-                }}
-            >
-                <DashboardIcon color={'#F4CE00'} />
-                <Typography marginLeft={1}>Dashboard</Typography>
-            </Button>
-            <Button
-                sx={{
-                    color: "#FFFFFF"
-                }}
-            >
-                <ProjectsIcon color={'#FFFFFF'} />
-                <Typography marginLeft={1}>Projects</Typography>
-            </Button>
-            <Button
-                sx={{
-                    color: "#FFFFFF",
-                    ":hover": {
-                        color: "hoverTab.main",
-                    },
-                }}
-            >
-                <ProfileIcon color={'#FFFFFF'} />
-                <Typography marginLeft={1}>Profile</Typography>
-            </Button>
-            <Button
-                sx={{
-                    color: "#FFFFFF",
-                    ":hover": {
-                        color: "hoverTab.main",
-                    },
-                }}
-            >
-                <CollaboratorsIcon color={'#FFFFFF'} />
-                <Typography marginLeft={1}> Collaborators</Typography>
-            </Button>
-            <Button
-                sx={{
-                    color: "#FFFFFF",
-                    ":hover": {
-                        color: "hoverTab.main",
-                    },
-                }}
-            >
-                <MessageIcon color={'#FFFFFF'} />
-                <Typography marginLeft={1}> Messages</Typography>
-            </Button>
-            <Button
-                sx={{
-                    color: "#FFFFFF",
-                    ":hover": {
-                        color: "hoverTab.main",
-                    },
-                }}
-            >
-                <SettingsIcon color={'#FFFFFF'} />
-                <Typography marginLeft={1}> Settings</Typography>
-            </Button>
-            <Button
-                sx={{
-                    color: "#FFFFFF",
-                    ":hover": {
-                        color: "hoverTab.main",
-                    },
-                }}
-            >
-                <GlobeIcon color={'#FFFFFF'} />
-                <Typography marginLeft={1}> Language</Typography>
-            </Button>
-        </Stack>
+        <Box onClick={onClick}>
+            {showMobileDashboard ? <MobileDash /> : <MenuIcon /> }
+        </Box>
     );
 }
+
+function MobileDash() {
+    return (
+    <Stack
+        spacing={3}
+        alignItems='flex-start'
+        sx={{
+            marginX: "25px",
+            marginY: "30px",
+            display: { sm: "none" },
+        }}
+    >
+        <Box width='100%' marginBottom={1}>
+            <SearchBox />
+        </Box>
+        <Button
+            sx={{
+                color: "#F4CE00",
+                ":hover": {
+                    backgroundColor: "hoverPrimary.main",
+                },
+            }}
+        >
+            <DashboardIcon color={"#F4CE00"} />
+            <Typography marginLeft={1}>Dashboard</Typography>
+        </Button>
+        <Button
+            sx={{
+                color: "#FFFFFF",
+            }}
+        >
+            <ProjectsIcon color={"#FFFFFF"} />
+            <Typography marginLeft={1}>Projects</Typography>
+        </Button>
+        <Button
+            sx={{
+                color: "#FFFFFF"
+            }}
+        >
+            <ProfileIcon color={"#FFFFFF"} />
+            <Typography marginLeft={1}>Profile</Typography>
+        </Button>
+        <Button
+            sx={{
+                color: "#FFFFFF"
+            }}
+        >
+            <CollaboratorsIcon color={"#FFFFFF"} />
+            <Typography marginLeft={1}> Collaborators</Typography>
+        </Button>
+        <Button
+            sx={{
+                color: "#FFFFFF"
+            }}
+        >
+            <MessageIcon color={"#FFFFFF"} />
+            <Typography marginLeft={1}> Messages</Typography>
+        </Button>
+        <Button
+            sx={{
+                color: "#FFFFFF"
+            }}
+        >
+            <SettingsIcon color={"#FFFFFF"} />
+            <Typography marginLeft={1}> Settings</Typography>
+        </Button>
+        <Button
+            sx={{
+                color: "#FFFFFF"
+            }}
+        >
+            <GlobeIcon color={"#FFFFFF"} />
+            <Typography marginLeft={1}> Language</Typography>
+        </Button>
+    </Stack>
+    )
+};
