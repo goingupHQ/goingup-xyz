03243395efc8f4a8fdfee1575441f86db292e8b9
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2022-07-29 02:27:43 -0700

    Profile page fade

--- web-app-2.0/pages/profile/[address].js
+++ web-app-2.0/pages/profile/[address].js
@@ -1,16 +1,12 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import Head from 'next/head';
-import possessive from '@wardrakus/possessive';
-import TopSection from '../../components/pages/profile/top-section';
-import {
-    Grid,
-    CardContent,
-    styled,
-} from '@mui/material';
-import { useRouter } from 'next/router';
-import Projects from '../projects';
-import Poaps from '../../components/pages/profile/poaps';
-import AppreciationTokens from '../../components/pages/profile/appreciation-tokens';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import possessive from "@wardrakus/possessive";
+import TopSection from "../../components/pages/profile/top-section";
+import { Grid, CardContent, styled, Fade, Box } from "@mui/material";
+import { useRouter } from "next/router";
+import Projects from "../projects";
+import Poaps from "../../components/pages/profile/poaps";
+import AppreciationTokens from "../../components/pages/profile/appreciation-tokens";
 
 const CardContentWrapper = styled(CardContent)(
     () => `
@@ -19,45 +15,48 @@ const CardContentWrapper = styled(CardContent)(
 );
 
 function ProfilePage() {
-    const [account, setAccount] = useState(null)
+    const [account, setAccount] = useState(null);
     const router = useRouter();
     const { address } = router.query;
-    console.log(router.query)
+    console.log(router.query);
 
     const getAccount = async () => {
         if (address) {
             const response = await fetch(`/api/get-account?address=${address}`);
             setAccount(await response.json());
         }
-    }
+    };
 
     useEffect(() => {
         getAccount();
-    }, [address])
+    }, [address]);
 
     return (
         <>
-            {account &&
-            <>
-                <Head>
-                    <title>{possessive(account?.name)} GoingUP Profile</title>
-                </Head>
+            {account && (
+                <>
+                    <Head>
+                        <title>
+                            {possessive(account?.name)} GoingUP Profile
+                        </title>
+                    </Head>
 
-                <Grid
-                    sx={{ px: { xs: 2, md: 4 }, marginBottom: 10 }}
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="stretch"
-                    spacing={3}
-                >
-                    <TopSection account={account} refresh={getAccount} />
-                    <Projects account={account} />
-                    <AppreciationTokens account={account} refresh={getAccount} />
-                    <Poaps account={account} refresh={getAccount} />
-                </Grid>
-            </>
-            }
+                    <Fade in={true} timeout={1000}>
+                        <Box fullWidth>
+                            <TopSection
+                                account={account}
+                                refresh={getAccount}
+                            />
+                            <Projects account={account} />
+                            <AppreciationTokens
+                                account={account}
+                                refresh={getAccount}
+                            />
+                            <Poaps account={account} refresh={getAccount} />
+                        </Box>
+                    </Fade>
+                </>
+            )}
         </>
     );
 }
