1086772f8a68865dd6df1434ff65213f17acceaf
Mark Ibanez <mark.ibanez@gmail.com>
2022-08-08 21:47:52 +0800

    Update project landing page

--- web-app-2.0/pages/projects/index.js
+++ web-app-2.0/pages/projects/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
-import { Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { AppContext } from '../../contexts/app-context';
 import { useContext } from 'react';
+import { WalletContext } from '../../contexts/wallet-context';
 
 export default function Projects() {
     const app = useContext(AppContext);
+    const wallet = useContext(WalletContext);
 
     return (
         <>
@@ -13,7 +15,14 @@ export default function Projects() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Typography variant="h1">Projects</Typography>
+            <Box sx={{ paddingTop: '20px' }}>
+                {wallet.address === null && (
+                    <Stack fullWidth justifyContent="center" alignItems="center" direction="column" spacing={4}>
+                        <Typography variant="h2">You need a connected wallet with a GoingUP account to access Projects</Typography>
+                        <img src="/images/illustrations/connection-lost.svg" alt='connection-lost' style={{ width: '100%', maxWidth: '500px'}} />
+                    </Stack>
+                )}
+            </Box>
         </>
     );
 }
