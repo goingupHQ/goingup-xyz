330644a73d41def7177047238c270a2e19d85335
Mark Ibanez <mark.ibanez@gmail.com>
2022-08-08 22:14:40 +0800

    Set price back to 2.22 eth

--- web-app-2.0/public/webflow/membership-mint.js
+++ web-app-2.0/public/webflow/membership-mint.js
@@ -62,8 +62,8 @@ const mint = async () => {
         const merkleProof = await response.json();
         try {
             const signerContract = new ethers.Contract(contractAddress, abi, signer);
-            // const tx = await signerContract.mint(merkleProof, { value: ethers.utils.parseEther('2.22') });
-            const tx = await signerContract.mint(merkleProof, { value: 1000000  });
+            const tx = await signerContract.mint(merkleProof, { value: ethers.utils.parseEther('2.22') });
+            // const tx = await signerContract.mint(merkleProof, { value: 1000000  });
             alert(`Mint transaction submitted to the blockchain. Please monitor your wallet for transaction result.`);
         } catch (err) {
             console.log(err);
