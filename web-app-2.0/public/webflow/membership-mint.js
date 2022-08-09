13e019df29ca43cfad536e981a46e77eac3453ea
Mark Ibanez <mark.ibanez@gmail.com>
2022-08-09 08:48:32 +0800

    Added minting UI hints

--- web-app-2.0/public/webflow/membership-mint.js
+++ web-app-2.0/public/webflow/membership-mint.js
@@ -40,39 +40,64 @@ web3Modal = new Web3Modal({
     theme: 'dark',
 });
 
+const disableMintButton = () => {
+    const mintButton = document.getElementById('mint-button');
+    mintButton.style.opacity = 0.5;
+    mintButton.style.pointerEvents = 'none';
+    mintButton.innerText = 'Minting...';
+}
+
+const enableMintButton = () => {
+    const mintButton = document.getElementById('mint-button');
+    mintButton.style.opacity = 1;
+    mintButton.style.pointerEvents = 'auto';
+    mintButton.innerText = 'Mint';
+}
+
 const mint = async () => {
-    document.querySelector('.web3modal-modal-lightbox').style.zIndex = 10000;
-    const web3ModalProvider = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+    disableMintButton();
 
-    const { chainId } = await provider.getNetwork();
+    try {
+        document.querySelector('.web3modal-modal-lightbox').style.zIndex = 10000;
+        const web3ModalProvider = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(web3ModalProvider);
 
-    if (chainId !== 1) {
-        alert('Please connect to the mainnet');
-        return;
-    }
+        const { chainId } = await provider.getNetwork();
 
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    const response = await fetch(
-        `https://goingup-xyz-dev.vercel.app/api/admin/membership-nft/get-merkle-proof?address=${address}`
-    );
+        if (chainId !== 1) {
+            alert('Please connect to the mainnet');
+            return;
+        }
 
-    if (response.status === 200) {
-        const merkleProof = await response.json();
-        try {
-            const signerContract = new ethers.Contract(contractAddress, abi, signer);
-            const tx = await signerContract.mint(merkleProof, { value: ethers.utils.parseEther('2.22') });
-            // const tx = await signerContract.mint(merkleProof, { value: 1000000  });
-            alert(`Mint transaction submitted to the blockchain. Please monitor your wallet for transaction result.`);
-        } catch (err) {
-            console.log(err);
-            if (err?.data?.originalError?.message === 'execution reverted: not whitelisted') {
-                alert('You are not whitelisted to mint NFTs');
-            } else {
-                alert('Something went wrong. Please contact GoingUP support.');
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        const response = await fetch(
+            `https://goingup-xyz-dev.vercel.app/api/admin/membership-nft/get-merkle-proof?address=${address}`
+        );
+
+        if (response.status === 200) {
+            const merkleProof = await response.json();
+            try {
+                const signerContract = new ethers.Contract(contractAddress, abi, signer);
+                const tx = await signerContract.mint(merkleProof, { value: ethers.utils.parseEther('2.22') });
+                // const tx = await signerContract.mint(merkleProof, { value: 1000000  });
+                alert(`Mint transaction submitted to the blockchain. Please monitor your wallet for transaction result.`);
+            } catch (err) {
+                const message = err.message;
+                console.log('error message', message);
+                if (message.includes('execution reverted: not whitelisted')) {
+                    alert('You are not whitelisted to mint');
+                } else if (message.includes('insufficient funds')) {
+                    alert('You do not have enough ether to mint');
+                } else {
+                    alert('Something went wrong. Please contact GoingUP support.');
+                }
             }
         }
+    } catch (err) {
+        alert('Something went wrong. Please contact GoingUP support.');
+    } finally {
+        enableMintButton();
     }
 };
 
