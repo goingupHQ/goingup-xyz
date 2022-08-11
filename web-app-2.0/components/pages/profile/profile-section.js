f6325eaf477159456f05bfeb89564f99d5fe6e8f
alfredoarriolajr <alfredoarriolajr@gmail.com>
2022-08-11 00:48:32 -0700

    truncate eth address

--- web-app-2.0/components/pages/profile/profile-section.js
+++ web-app-2.0/components/pages/profile/profile-section.js
@@ -14,6 +14,7 @@ import {
     Button,
 } from "@mui/material";
 import ContactsAndIntegrations from "./contacts-and-integrations";
+import truncateEthAddress from 'truncate-eth-address'
 import { useTheme } from "@mui/material";
 
 const ProfileSection = (props) => {
@@ -201,7 +202,7 @@ const ProfileSection = (props) => {
                                                     : "#CFCFCF",
                                         }}
                                     >
-                                        {account.address}
+                                        {truncateEthAddress(account.address)}
                                     </Typography>
                                 </Stack>
                             </Grid>
