a19493f14eb14d5247ab2dd55b88160b7734e3c8
alfredoarriolajr <alfredoarriolajr@gmail.com>
2022-08-13 23:58:03 -0700

    Profile detail margin update

--- web-app-2.0/components/pages/profile/profile-section.js
+++ web-app-2.0/components/pages/profile/profile-section.js
@@ -226,7 +226,7 @@ const ProfileSection = (props) => {
                                 </Typography>
                             </Stack>
                         </Grid>
-                        <Typography>
+                        <Typography marginX={3}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit, sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua. Ut enim ad minim veniam, quis
