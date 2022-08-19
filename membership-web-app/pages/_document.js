ce667e0f29ca138ff71fd87e7da376aa70c79724
Mark Ibanez <mark.ibanez@gmail.com>
2022-08-19 09:47:30 +0800

    Update _document.js

--- membership-web-app/pages/_document.js
+++ membership-web-app/pages/_document.js
@@ -1,9 +1,11 @@
+/* eslint-disable */
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
         <Html>
             <Head>
+
                 <title>GoingUP</title>
                 <meta name="description" content="GoingUP Membership NFTs" />
                 <link rel="icon" href="/favicon.png" />
