75b81db85dbb4f4675ea13f1ed6154d185f02f41
Mark Ibanez <mark.ibanez@gmail.com>
2022-08-20 11:27:25 +0800

    Implement CORS for purchase endpoint

--- web-app-2.0/pages/api/admin/membership-nft/purchase.js
+++ web-app-2.0/pages/api/admin/membership-nft/purchase.js
@@ -1,16 +1,24 @@
 import { ethers } from 'ethers';
 import { getDb } from '../../_get-db-client';
+import NextCors from 'nextjs-cors';
 
 export default async function handler(req, res) {
-    const account = req.query.account?.toLowerCase();
+    // Run the cors middleware
+    // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
+    const account = req.query.account?.toLowerCase();
     if (!account) {
         res.status(400).send('Missing account');
         return;
     }
 
     const txHash = req.query.txhash;
-
     if (!txHash) {
         res.status(400).send('Missing txhash');
         return;
