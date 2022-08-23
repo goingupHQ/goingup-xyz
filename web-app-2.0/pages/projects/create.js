8ae0947c0b4b91e297e2e3379406b5b19a959921
Mark Ibanez <mark.ibanez@gmail.com>
2022-08-23 22:48:59 +0800

    Completed initial create project page

--- web-app-2.0/pages/projects/create.js
+++ web-app-2.0/pages/projects/create.js
@@ -1,206 +1,206 @@
 import {
-  Alert,
-  Box,
-  Button,
-  Checkbox,
-  Grid,
-  Typography,
-  Stack,
-  Snackbar,
-  TextField,
-  Modal,
-} from "@mui/material";
-import Head from "next/head";
-import React, { useState, useContext } from "react";
-import {
-  useContract,
-  useSigner
-} from "wagmi";
-import { mumbaiAddress, projectsAbi } from "../../contexts/projects-context";
-import { AppContext } from "../../contexts/app-context";
-import DatePicker from "../../components/ui/datepicker";
-import {BigNumber} from 'ethers';
+    Alert,
+    Box,
+    Button,
+    Checkbox,
+    Grid,
+    Typography,
+    Stack,
+    Snackbar,
+    TextField,
+    Modal,
+    Autocomplete,
+    Chip,
+    FormControlLabel,
+    Backdrop,
+    CircularProgress,
+    Paper,
+} from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import Head from 'next/head';
+import React, { useState, useContext } from 'react';
+import { useContract, useSigner } from 'wagmi';
+import { mumbaiAddress, projectsAbi, ProjectsContext } from '../../contexts/projects-context';
+import { AppContext } from '../../contexts/app-context';
+import DatePicker from '../../components/ui/datepicker';
+import { BigNumber } from 'ethers';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers';
+import { useSnackbar } from 'notistack';
+import { useRouter } from 'next/router';
 
 export default function CreateProject(props) {
-  const app = useContext(AppContext);
+    const app = useContext(AppContext);
+    const projectsCtx = useContext(ProjectsContext);
 
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [started, setStarted] = useState("");
-  const [ended, setEnded] = useState("");
-  const [primaryUrl, setPrimaryUrl] = useState("");
-  const [tags, setTags] = useState([]);
-  const [isPrivate, setIsPrivate] = useState("");
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [started, setStarted] = useState(null);
+    const [ended, setEnded] = useState(null);
+    const [primaryUrl, setPrimaryUrl] = useState('');
+    const [tags, setTags] = useState([]);
+    const [isPrivate, setIsPrivate] = useState('');
 
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const [creating, setCreating] = useState(false);
+    const [tx, setTx] = useState(null);
 
-  // console.log(app);
-  const modalStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "white",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const router = useRouter();
 
-  const { data: signer } = useSigner();
+    const createProject = async () => {
+        closeSnackbar();
+        setTx(null);
+        setCreating(true);
 
-  const contract = useContract({
-    addressOrName: mumbaiAddress,
-    contractInterface: projectsAbi,
-    signerOrProvider: signer,
-    functionName: "create",
-    chainId: 80001,
-    watch: true,
-  });
+        try {
+            enqueueSnackbar('Creating transactions, please approve on your wallet', { variant: 'info', persist: true });
+            const createTx = await projectsCtx.createProject(
+                name,
+                description,
+                started,
+                ended,
+                primaryUrl,
+                tags,
+                isPrivate
+            );
+            closeSnackbar();
 
-  return (
-    <>
-      <Head>
-        <title>Going UP - Create A Project</title>
-      </Head>
+            enqueueSnackbar('Waiting for transaction confirmations', {
+                variant: 'info',
+                persist: true,
+                action: (key) => {
+                    <Button onClick={() =>
+                    {
+                        const baseUrl = projectsCtx.networkParams.blockExplorers[0];
+                        window.open(`${baseUrl}tx/${createTx.hash}`);
+                    }}>
+                        Show Transaction
+                    </Button>
+                },
+            });
+            setTx(createTx);
+            await createTx.wait();
+            closeSnackbar();
 
-      <Stack spacing="2rem" justify="center">
-        <Typography variant="h1">Create A Project</Typography>
-
-        <Stack width={{ xs: "100%", md: "50%" }} spacing="0.75rem">
-          <Typography variant="h2">Project Name</Typography>
-          <TextField
-            onChange={(e) => setName(e.target.value)}
-            id="outlined-basic"
-            label="Name"
-            variant="outlined"
-            value={name}
-            placeholder="Project Example Name"
-          />
-        </Stack>
-
-        <Stack width={{ xs: "100%", md: "50%" }} spacing="0.75rem">
-          <Typography variant="h2">Project Description</Typography>
-          <TextField
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            id="outlined-basic"
-            label="Description"
-            variant="outlined"
-            multiline
-            rows={4}
-            placeholder="This project is about..."
-          />
-
-          <Stack
-            p={1}
-            border={
-              new Date(ended) <= new Date(started) ? "2px solid red" : "none"
+            enqueueSnackbar('Project created', { variant: 'success' });
+            router.push('/projects');
+        } catch (e) {
+            closeSnackbar();
+            if (typeof e === 'string') {
+                enqueueSnackbar(e, { variant: 'error' });
+            } else {
+                enqueueSnackbar(e.message || `Sorry something went wrong`, { variant: 'error' });
             }
-            borderRadius="16px"
-            spacing="16px"
-          >
-            <DatePicker
-              label={`Started`}
-              value={started}
-              onChange={(e) => setStarted(BigNumber.from(parseInt(e.target.value)).toString())}
-              placeholder={"YYYY-MM-DD"}
-            />
+        } finally {
+            setTx(null);
+            setCreating(false);
+        }
+    };
 
-            <DatePicker
-              label={`Ended`}
-              value={ended}
-              onChange={(e) => setEnded(BigNumber.from(parseInt(e.target.value)).toString())}
-              placeholder={"YYYY-MM-DD"}
-            />
-            {new Date(ended) <= new Date(started) && (
-              <Alert variant="filled" severity="error">
-                End date must be after start date
-              </Alert>
-            )}
-          </Stack>
-        </Stack>
-        <Stack width={{ xs: "100%", md: "50%" }} spacing="0.75rem">
-          <Typography variant="h2">Project Link</Typography>
-          <TextField
-            onChange={(e) => setPrimaryUrl(e.target.value)}
-            value={primaryUrl}
-            id="outlined-basic"
-            label="Primary URL"
-            variant="outlined"
-            placeholder="https://www.project.com"
-          />
-        </Stack>
-        <Stack width={{ xs: "100%", md: "50%" }} spacing={2}>
-          <Typography variant="h2">Project Tags</Typography>
-          <Button variant="outlined" onClick={handleOpen}>
-            Select Tags
-          </Button>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={modalStyle}>
-              <Grid columns={{ xs: 4, sm: 8, md: 12 }} spacing={3}>
-                {app.occupations.map((occupation) => (
-                  <Button
-                    key={occupation.id}
-                    p="0.5rem"
-                    variant="contained"
-                    onClick={() =>
-                      !tags.includes(occupation.text) &&
-                      setTags([...tags, occupation.text])
-                    }
-                    margin={1}
-                  >
-                    <Typography color="black" variant="h3">
-                      {occupation.text}
-                    </Typography>
-                  </Button>
-                ))}
-              </Grid>
-            </Box>
-          </Modal>
-          <Grid spacing="16px">
-            {" "}
-            {tags.map((tag, id) => (
-              <Button
-                key={id}
-                p="0.5rem"
-                variant="outlined"
-                onClick={() => setTags(tags.filter((item) => item !== tag))}
-                marginLeft="8px"
-              >
-                <Typography variant="h3">{tag}</Typography>
-              </Button>
-            ))}
-          </Grid>
-          <Checkbox 
-          onChange={(e) => setIsPrivate(e.target.checked)}
-          label="Is it a private project?" defaultChecked />
+    return (
+        <>
+            <Head>
+                <title>Going UP - Create A Project</title>
+            </Head>
 
-        </Stack>
+            <Stack spacing={1} sx={{ width: { xs: '100%', md: '60%', lg: '50%', xl: '40%' } }}>
+                <Typography variant="h1" sx={{ paddingLeft: 2 }}>
+                    Create A Project
+                </Typography>
 
+                <Grid container columnSpacing={2} rowSpacing={2} sx={{ padding: 0 }}>
+                    <Grid item xs={12}>
+                        <TextField
+                            onChange={(e) => setName(e.target.value)}
+                            id="outlined-basic"
+                            label="Project Name"
+                            variant="outlined"
+                            value={name}
+                            fullWidth
+                        />
+                    </Grid>
 
-        <Button variant="contained" onClick={() => contract.create(name, description, started, ended, primaryUrl, JSON.stringify(tags), isPrivate)}>
-          Create Project
-        </Button>
-        {/* {error && (
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert
-              onClose={handleClose}
-              severity="error"
-              sx={{ width: "100%" }}
-            >
-              An error occurred preparing the transaction: {error.message}
-            </Alert>
-          </Snackbar>
-        )} */}
-      </Stack>
-    </>
-  );
+                    <Grid item xs={12}>
+                        <TextField
+                            onChange={(e) => setDescription(e.target.value)}
+                            value={description}
+                            id="outlined-basic"
+                            label="Project Description"
+                            variant="outlined"
+                            multiline
+                            rows={4}
+                            placeholder="This project is about..."
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <DesktopDatePicker
+                            label="Started"
+                            inputFormat="MM/DD/yyyy"
+                            value={started}
+                            onChange={(newValue) => setStarted(newValue)}
+                            renderInput={(params) => <TextField {...params} autoComplete={false} fullWidth />}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <DesktopDatePicker
+                            label="Ended"
+                            inputFormat="MM/DD/yyyy"
+                            value={ended}
+                            onChange={(newValue) => setEnded(newValue)}
+                            renderInput={(params) => <TextField {...params} autoComplete={false} fullWidth />}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField
+                            onChange={(e) => setPrimaryUrl(e.target.value)}
+                            value={primaryUrl}
+                            id="outlined-basic"
+                            label="Primary URL"
+                            variant="outlined"
+                            placeholder="https://www.project.com"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Autocomplete
+                            multiple
+                            // id="tags-filled"
+                            options={[]}
+                            // defaultValue={[top100Films[13].title]}
+                            onChange={(e, value) => setTags(value)}
+                            freeSolo
+                            renderTags={(value, getTagProps) =>
+                                value.map((option, index) => (
+                                    <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                                ))
+                            }
+                            renderInput={(params) => <TextField {...params} variant="outlined" label="Tags" />}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <FormControlLabel
+                            label="Is this a private project?"
+                            control={<Checkbox checked={isPrivate} onChange={(e) => setIsPrivate(e.target.checked)} />}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <LoadingButton variant="contained" onClick={createProject}>
+                            Create Project
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
+            </Stack>
+
+            <Backdrop open={creating} sx={{ opacity: 1 }}>
+                <CircularProgress />
+            </Backdrop>
+        </>
+    );
 }
