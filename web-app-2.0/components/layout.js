40cbe9f4fd3a17545bbe1aa497eb67db71be8f12
Mark Ibanez <mark.ibanez@gmail.com>
2022-09-03 23:12:31 +0800

    Update layout.js

--- web-app-2.0/components/layout.js
+++ web-app-2.0/components/layout.js
@@ -1,9 +1,9 @@
-import { Padding } from "@mui/icons-material";
-import { Box, createTheme, ThemeProvider } from "@mui/material";
-import { useContext, useEffect, useMemo } from "react";
-import { AppContext } from "../contexts/app-context";
-import { WalletContext } from "../contexts/wallet-context";
-import Header from "./layout/header";
+import { Padding } from '@mui/icons-material';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
+import { useContext, useEffect, useMemo } from 'react';
+import { AppContext } from '../contexts/app-context';
+import { WalletContext } from '../contexts/wallet-context';
+import Header from './layout/header';
 
 export default function Layout({ children }) {
     const app = useContext(AppContext);
@@ -15,156 +15,155 @@ export default function Layout({ children }) {
                 palette: {
                     mode: app.mode,
                     primary: {
-                        main: "#F4CE00",
+                        main: '#F4CE00',
                     },
                     background: {
-                        paper: app.mode === "dark" ? "#111921" : "#F5F5F5",
-                        dark: "#0F151C",
-                        light: "#FFFFFF",
-                        default: app.mode === "dark" ? "#0F151C" : "#FFFFFF",
-                        main: app.mode === "dark" ? "#0F151C" : "#FFFFFF",
-                        searchBar: app.mode === "dark" ? "#19222C" : "#F5F5F5",
-                        userBox: app.mode === "dark" ? "#19222C" : "#EDEFED",
+                        paper: app.mode === 'dark' ? '#111921' : '#F5F5F5',
+                        dark: '#0F151C',
+                        light: '#FFFFFF',
+                        default: app.mode === 'dark' ? '#0F151C' : '#FFFFFF',
+                        main: app.mode === 'dark' ? '#0F151C' : '#FFFFFF',
+                        searchBar: app.mode === 'dark' ? '#19222C' : '#F5F5F5',
+                        userBox: app.mode === 'dark' ? '#19222C' : '#EDEFED',
                     },
                     hoverPrimary: {
-                        main: "#FFE555",
+                        main: '#FFE555',
                     },
                     activePrimary: {
-                        main: "#FFF4B8",
+                        main: '#FFF4B8',
                     },
                     secondary: {
-                        main: "#7A55FF",
+                        main: '#7A55FF',
                     },
                     error: {
-                        main: "#FF8199",
+                        main: '#FF8199',
                     },
                     text1: {
-                        main: "#FFFFFF",
+                        main: '#FFFFFF',
                     },
                     background1: {
-                        main: "#0F151C",
+                        main: '#0F151C',
                     },
                     background2: {
-                        main: "#19222C",
+                        main: '#19222C',
                     },
                     backgroundLight: {
-                        main: "#FFFFFF",
+                        main: '#FFFFFF',
                     },
                     hoverFollow: {
-                        main: "#5537C3",
-                        text: "#F4CE00",
+                        main: '#5537C3',
+                        text: '#F4CE00',
                     },
                     activeFollow: {
-                        main: "#3AB795",
-                        text: "#FFFFFF",
+                        main: '#3AB795',
+                        text: '#FFFFFF',
                     },
                     hoverViewAllProjects: {
-                        main: "#F4CE00",
+                        main: '#F4CE00',
                     },
                     activeViewAllProjects: {
-                        main: "#D2B200",
+                        main: '#D2B200',
                     },
                     hoverTabDark: {
-                        main: "#151E26",
+                        main: '#151E26',
                     },
                     hoverTabLight: {
-                        main: "#F5F5F5",
+                        main: '#F5F5F5',
                     },
                     hoverListDark: {
-                        main: "#F4CE00",
+                        main: '#F4CE00',
                     },
                     hoverListLight: {
-                        main: "#378CDB",
+                        main: '#378CDB',
                     },
                     formBorder: {
-                        main: "#FF8199",
+                        main: '#FF8199',
                     },
                     text: {
-                        main: app.mode === "dark" ? "#FFFFFF" : "#010101",
+                        main: app.mode === 'dark' ? '#FFFFFF' : '#010101',
                     },
                     icon: {
-                        main: app.mode === "dark" ? "#4D5F72" : "#4D5F72",
-                        contacts: app.mode === "dark" ? "#FFFFFF" : "#081724",
-                        contactsBackground:
-                            app.mode === "dark" ? "#253340" : "#CFCFCF",
+                        main: app.mode === 'dark' ? '#4D5F72' : '#4D5F72',
+                        contacts: app.mode === 'dark' ? '#FFFFFF' : '#081724',
+                        contactsBackground: app.mode === 'dark' ? '#253340' : '#CFCFCF',
                     },
                     hoverTab: {
-                        main: "#4D5F72",
+                        main: '#4D5F72',
                     },
                     sidebar: {
-                        main: "#394859",
+                        main: '#394859',
                     },
                 },
                 typography: {
-                    fontFamily: "Questrial",
+                    fontFamily: 'Questrial',
                     h1: {
-                        fontSize: "32px",
-                        fontWeight: "bold",
+                        fontSize: '32px',
+                        fontWeight: 'bold',
                     },
                     h2: {
-                        fontSize: "24px",
-                        fontWeight: "bold",
+                        fontSize: '24px',
+                        fontWeight: 'bold',
                     },
                     h3: {
-                        fontSize: "18px",
-                        fontWeight: "bold",
+                        fontSize: '18px',
+                        fontWeight: 'bold',
                     },
                     sh1: {
-                        fontSize: "16px",
-                        "@media (max-width:900px)": {
-                            fontSize: "12px",
+                        fontSize: '16px',
+                        '@media (max-width:900px)': {
+                            fontSize: '12px',
                         },
-                        fontWeight: "500",
+                        fontWeight: '500',
                     },
                     sh2: {
-                        fontSize: "14px",
-                        "@media (max-width:900px)": {
-                            fontSize: "10px",
+                        fontSize: '14px',
+                        '@media (max-width:900px)': {
+                            fontSize: '10px',
                         },
-                        fontWeight: "500",
+                        fontWeight: '500',
                     },
                     sh3: {
-                        fontSize: "12px",
-                        fontWeight: "500",
+                        fontSize: '12px',
+                        fontWeight: '500',
                     },
                     p: {
-                        fontSize: "16px",
-                        fontWeight: "medium",
+                        fontSize: '16px',
+                        fontWeight: 'medium',
                     },
                     button: {
-                        textTransform: "capitalize",
-                        fontWeight: "bold",
+                        textTransform: 'capitalize',
+                        fontWeight: 'bold',
                     },
                     allVariants: {
-                        color: app.mode === "dark" ? "#FFFFFF" : "#010101",
+                        color: app.mode === 'dark' ? '#FFFFFF' : '#010101',
                     },
                     rep: {
-                        fontSize: "15px",
-                        "@media (max-width:900px)": {
-                            fontSize: "7px",
+                        fontSize: '15px',
+                        '@media (max-width:900px)': {
+                            fontSize: '7px',
                         },
-                        fontWeight: "500",
+                        fontWeight: '500',
                     },
                     walletText: {
-                        fontSize: "12px",
-                        "@media (max-width:900px)": {
-                            fontSize: "10px",
+                        fontSize: '12px',
+                        '@media (max-width:900px)': {
+                            fontSize: '10px',
                         },
-                        fontWeight: "500",
+                        fontWeight: '500',
                     },
                     mobileh1: {
-                        fontSize: "32px",
-                        "@media (max-width:900px)": {
-                            fontSize: "18px",
+                        fontSize: '32px',
+                        '@media (max-width:900px)': {
+                            fontSize: '18px',
                         },
-                        fontWeight: "bold",
+                        fontWeight: 'bold',
                     },
                     mobileh2: {
-                        fontSize: "16px",
-                        "@media (max-width:900px)": {
-                            fontSize: "14px",
+                        fontSize: '16px',
+                        '@media (max-width:900px)': {
+                            fontSize: '14px',
                         },
-                        fontWeight: "bold",
+                        fontWeight: 'bold',
                     },
                 },
                 shape: {
@@ -183,8 +182,7 @@ export default function Layout({ children }) {
                     MuiAppBar: {
                         defaultProps: {
                             sx: {
-                                backgroundColor:
-                                    app.mode === "dark" ? "#0F151C" : "#FFFFFF",
+                                backgroundColor: app.mode === 'dark' ? '#0F151C' : '#FFFFFF',
                                 // height: '80px',
                             },
                             elevation: 0,
@@ -197,7 +195,7 @@ export default function Layout({ children }) {
                     },
                     MuiDivider: {
                         defaultProps: {
-                            color: app.mode === "dark" ? "#25303C" : "#E7E7E7",
+                            color: app.mode === 'dark' ? '#25303C' : '#E7E7E7',
                         },
                     },
                     MuiChip: {
@@ -208,8 +206,8 @@ export default function Layout({ children }) {
                     MuiToolbar: {
                         defaultProps: {
                             sx: {
-                                paddingTop: { xs: "15px", md: "20px" },
-                                paddingX: { xs: "15px", lg: "105px" },
+                                paddingTop: { xs: '15px', md: '20px' },
+                                paddingX: { xs: '15px', lg: '105px' },
                             },
                         },
                     },
@@ -229,15 +227,13 @@ export default function Layout({ children }) {
     useEffect(() => {
         // update body background color on theme change
         document.body.style.backgroundColor =
-            app.mode === "dark"
-                ? theme.palette.background.dark
-                : theme.palette.background.light;
+            app.mode === 'dark' ? theme.palette.background.dark : theme.palette.background.light;
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [app.mode]);
 
     useEffect(() => {
         // connect wallet if cached
-        if (localStorage.getItem("wallet-context-cache")) wallet.connect();
+        if (localStorage.getItem('wallet-context-cache')) wallet.connect();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
@@ -247,8 +243,9 @@ export default function Layout({ children }) {
                 <Header />
                 <Box
                     sx={{
-                        paddingTop: { xs: "74px", md: "150px" },
-                        paddingX: { xs: "15px", lg: "105px" },
+                        paddingTop: { xs: '74px', md: '150px' },
+                        paddingBottom: 5,
+                        paddingX: { xs: '15px', lg: '105px' },
                     }}
                 >
                     {children}
