83620da0a17bf94dd3cae07e6f4acb7d16306f51
Mark Ibanez <mark.ibanez@gmail.com>
2022-09-04 11:53:36 +0800

    Add ENS reverse resolution

--- web-app-2.0/components/common/account-name-address.js
+++ web-app-2.0/components/common/account-name-address.js
@@ -2,10 +2,13 @@ import { ethers } from 'ethers';
 import truncateEthAddress from 'truncate-eth-address';
 import React from 'react';
 import NextLink from 'next/link';
+import { WalletContext } from '../../contexts/wallet-context';
 
 export default function AccountNameAddress(props) {
     const { address } = props;
     const [name, setName] = React.useState(null);
+    const [ensName, setEnsName] = React.useState(null);
+    const wallet = React.useContext(WalletContext);
 
     const displayAddress = ethers.utils.isAddress(address) ? truncateEthAddress(address) : 'Invalid Address';
 
@@ -14,13 +17,14 @@ export default function AccountNameAddress(props) {
             fetch(`/api/get-account-name?address=${address}`).then(async (res) => {
                 const result = await res.text();
                 setName(result);
+                setEnsName(await wallet.mainnetENSProvider.lookupAddress(address));
             });
         }
     }, [address]);
 
     return (
         <NextLink href={`/profile/${address}`}>
-            {name === null ? displayAddress : `${name} (${displayAddress})`}
+            {name === null ? displayAddress : `${name} (${ensName || displayAddress})`}
         </NextLink>
     );
 }
