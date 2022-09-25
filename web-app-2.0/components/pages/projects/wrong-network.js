ccc4d3ccec15b9f44e90f30183d723a947ca86d6
Mark Ibanez <mark.ibanez@gmail.com>
2022-09-25 09:44:21 +0800

    Refactor wrong network component

---
+++ web-app-2.0/components/pages/projects/wrong-network.js
@@ -0,0 +1,24 @@
+import { Button, Stack, Typography } from '@mui/material';
+import React from 'react';
+import { ProjectsContext } from '../../../contexts/projects-context';
+
+export default function WrongNetwork(props) {
+    const projectsContext = React.useContext(ProjectsContext);
+    return (
+        <Stack justifyContent="center" alignItems="center" direction="column" spacing={4}>
+            <Typography variant="h2">
+                You need to switch to {projectsContext.networkParams.chainName} to access Projects
+            </Typography>
+
+            <img
+                src="/images/illustrations/page-lost.svg"
+                alt="connection-lost"
+                style={{ width: '100%', maxWidth: '500px' }}
+            />
+
+            <Button variant="contained" color="primary" size="large" onClick={projectsContext.switchToCorrectNetwork}>
+                Switch to {projectsContext.networkParams.chainName}
+            </Button>
+        </Stack>
+    );
+}
