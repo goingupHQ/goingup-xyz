355e484f525b4af2c7b17d9006cfa7b99852c245
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2022-09-29 00:59:19 -0700

    Sent Tokens shows message name and address

--- web-app-2.0/components/pages/profile/sent-appreciation-tokens.js
+++ web-app-2.0/components/pages/profile/sent-appreciation-tokens.js
@@ -3,21 +3,17 @@ import { ethers } from "ethers";
 import React, { useContext, useEffect, useState } from "react";
 import { WalletContext } from "../../../contexts/wallet-context";
 import artifact from "../../../../artifacts/GoingUpUtilityToken.json";
-import Router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
 import truncateEthAddress from "truncate-eth-address";
+import AccountNameAddress from "../../common/account-name-address";
 
 export default function SentAppreciationTokenCard(props) {
-    const account = props;
-    const { tier, balance } = props;
     const wallet = useContext(WalletContext);
     const [messages, setMessages] = useState([]);
-    const [sentToAddress, setSentToAddress] = useState("");
-    const [sentToName, setSentToName] = useState("");
     const router = useRouter();
 
     useEffect(() => {
         getMessages();
-        getSenderAccountName();
     }, []);
 
     const contractAddress = wallet.utilityToken.address;
@@ -37,42 +33,23 @@ export default function SentAppreciationTokenCard(props) {
         const messagesResult = writeMintLogs.map((log) => {
             const parsedLog = _interface.parseLog(log);
             const message = { ...parsedLog.args };
+            console.log('message', message);
             const sender = message[2];
             const senderMessage = message[3];
             const senderAddress = sender === router.query.address;
             const sentTo = message.to;
-            setSentToAddress(sentTo);
             const messageTo = (
                 <Typography>
-                    {senderMessage} - {truncateEthAddress(sentTo)}
+                    {senderMessage} to - 
+                     <AccountNameAddress address={sentTo} /> {truncateEthAddress(sentTo)}
+
                 </Typography>
             );
             if (senderAddress) {
                 return messageTo;
             }
-            const names = getSenderAccountName(sentTo);
-            console.log("names", names);
-            if (names) {
-                sentTo === account.address;
-                console.log("sentToName", names);
-            }
         });
         setMessages(messagesResult);
-    };
-
-    const getSenderAccountName = async (address) => {
-        if (address) {
-            const response = await fetch(
-                `/api/get-account-name?address=${address}`
-            );
-            if (response.status === 200) {
-                const data = await response.text();
-                console.log("data", data);
-                return data;
-            } else {
-                return null;
-            }
-        }
     };
 
     return (
