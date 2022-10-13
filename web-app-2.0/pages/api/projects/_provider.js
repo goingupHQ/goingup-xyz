c1d33960ea41337a19d9b7f504c52b24483f19bb
Mark Ibanez <mark.ibanez@gmail.com>
2022-10-13 11:49:04 +0800

    Switch to testnet

--- web-app-2.0/pages/api/projects/_provider.js
+++ web-app-2.0/pages/api/projects/_provider.js
@@ -1,13 +1,13 @@
 import { ethers } from "ethers";
 
 // testnet
-// export const contractAddress = "0xF5df032832cb3c4BEf2D28B440fA57D5dAC47881";
-// export const contractNetwork = 80001;
-// export const alchemyKey = process.env.ALCHEMY_POLYGON_TESTNET;
+export const contractAddress = "0x89e41C41Fa8Aa0AE4aF87609D3Cb0F466dB343ab";
+export const contractNetwork = 80001;
+export const alchemyKey = process.env.ALCHEMY_POLYGON_TESTNET;
 
 // mainnet
-export const contractAddress = '0xb6b83BaE8251d305FcbdaF2aE8cDffAC39216C95';
-export const contractNetwork = 137;
-export const alchemyKey = process.env.ALCHEMY_POLYGON_MAINNET;
+// export const contractAddress = '0xb6b83BaE8251d305FcbdaF2aE8cDffAC39216C95';
+// export const contractNetwork = 137;
+// export const alchemyKey = process.env.ALCHEMY_POLYGON_MAINNET;
 
 export const provider = new ethers.providers.AlchemyProvider(contractNetwork, alchemyKey);
