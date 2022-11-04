31caaba04ec6380f351470fd204eb6555897a831
Mark Ibanez <mark.ibanez@gmail.com>
2022-11-04 11:04:35 +0800

    Remove old web-app project

--- web-app/pages/api/initialize-reputation-score.ts
+++
@@ -1,9 +0,0 @@
-import { getDb } from './_get-db-client';
-
-export default async function handler(req, res) {
-    console.log(req.query);
-    const db = await getDb();
-    const accounts = db.collection('accounts');
-    await accounts.updateMany({ reputationScore: { $exists: false }}, { $set: { reputationScore: 50 }});
-    res.status(200).send('reputation-scores-initialized');
-}
