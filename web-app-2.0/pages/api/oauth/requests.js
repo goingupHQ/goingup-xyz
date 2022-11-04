946610957fc779c26dda747a15f9d5a8e2ee8230
Mark Ibanez <mark.ibanez@gmail.com>
2022-11-04 13:59:45 +0800

    Update requests.js
    Fix build error by replacing obsolete dbClient import

--- web-app-2.0/pages/api/oauth/requests.js
+++ web-app-2.0/pages/api/oauth/requests.js
@@ -1,14 +1,13 @@
-import { dbClient } from '../_get-db-client';
+import { getDb } from '../_get-db-client';
 import { validateSignature } from '../_validate-signature';
 
 export default async function handler(req, res) {
-    await dbClient.connect();
     const { address, uuid, type, message, signature } = req.query;
 
     const isSignatureValid = validateSignature(address, `connect-${type}`, signature);
 
     if (isSignatureValid) {
-        const db = await dbClient.db();
+        const db = await getDb();
         const oauthRequests = db.collection('oauth-requests');
         await oauthRequests.insertOne({
             uuid, address, type
