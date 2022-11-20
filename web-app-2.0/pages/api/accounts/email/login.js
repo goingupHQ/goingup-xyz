9b8a098b17e9be44f97044390aaf7c117f9d9349
Mark Ibanez <mark.ibanez@gmail.com>
2022-11-20 19:16:45 +0800

    Update login.js
    Set cookie instead of sending access token

--- web-app-2.0/pages/api/accounts/email/login.js
+++ web-app-2.0/pages/api/accounts/email/login.js
@@ -9,10 +9,15 @@ export default async (req, res) => {
         return;
     }
 
-    const key = crypto.randomBytes(64).toString('hex');
+    const accessToken = crypto.randomBytes(64).toString('hex');
 
     // expires 100 days from now
     const expires = new Date(Date.now() + 100 * 24 * 60 * 60 * 1000);
 
-    res.status(200).json({ key, expires });
+    // set access token as secure cookie
+    res.setHeader('Set-Cookie', [
+        `access_token=${accessToken}; HttpOnly; Secure; SameSite=Strict; Expires=${expires.toUTCString()}`,
+    ]);
+
+    res.status(200).json({ authenticated: true });
 };
