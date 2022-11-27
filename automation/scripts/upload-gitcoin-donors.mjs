f9d9d6c7e4486e16eca4b5e1a568d61773ec1c77
Mark Ibanez <mark.ibanez@gmail.com>
2022-11-27 17:53:11 +0800

    Importing gitcoin addresses to database

---
+++ automation/scripts/upload-gitcoin-donors.mjs
@@ -0,0 +1,19 @@
+import csv from 'csvtojson';
+import { ethers } from 'ethers';
+import { getDb } from '../get-db-client.mjs';
+
+// get donor wallet addresses from csv file
+const csvFilePath = './scripts/gitcoin-donors.csv';
+const csvData = await csv().fromFile(csvFilePath);
+
+// fix addresses to be checksummed
+const addresses = csvData.map((row) => {
+    return ethers.utils.getAddress(row.donor_address);
+});
+
+// insert addresses into database if they don't already exist
+const db = await getDb();
+for (const address of addresses) {
+    await db.collection('donors').updateOne({ address }, { $set: { address } }, { upsert: true });
+    console.log(`Inserted ${address} into database`);
+}
