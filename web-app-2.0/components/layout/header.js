0819c1f40205cd1196894b99a5dcab36d224d720
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2022-12-15 01:03:05 -0800

    layout fix

--- web-app-2.0/components/layout/header.js
+++ web-app-2.0/components/layout/header.js
@@ -23,7 +23,7 @@ export default function Header(props) {
 
     return (
         <>
-            <AppBar sx={{ boxShadow: 6, paddingX: { lg: '85px' } }}>
+            <AppBar sx={{ boxShadow: 6, paddingX: { md: '45px' } }}>
                 <Toolbar sx={{ paddingY: 0 }}>
                     <Grid container spacing={1} alignItems="center" justifyContent="space-between">
                         <Grid item xs={5}>
@@ -78,7 +78,7 @@ export default function Header(props) {
                         marginTop: '12px'
                     }}
                 /> */}
-                <DesktopNav />
+                {/* <DesktopNav /> */}
                 <Fade direction="down" in={drawerOpen} mountOnEnter unmountOnExit>
                     <Box sx={{ paddingBottom: '20px', paddingX: '32px', zIndex: 100 }}>
                         <MobileNav closeNav={() => { setDrawerOpen(false) }} />
