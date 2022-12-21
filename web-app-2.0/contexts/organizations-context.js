cbef97e847f12e1b74b1e4814611414fe419e7b5
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2022-12-21 00:50:56 -0800

    removed static data and fetch from api

--- web-app-2.0/contexts/organizations-context.js
+++ web-app-2.0/contexts/organizations-context.js
@@ -1,50 +1,23 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { AppContext } from './app-context';
 
 export const OrganizationsContext = createContext({});
 
 export function OrganizationsProvider({ children }) {
     const app = useContext(AppContext);
-    const organizations = [
-        {
-            name: 'GoingUp',
-            description: 'Web3 Protocol for reputation & identity description',
-            address: '1',
-            logo: '/images/goingup-glyph.png',
-        },
-        {
-            name: 'GoingUp',
-            description: 'Web3 Protocol for reputation & identity description',
-            address: '2',
-            logo: '/images/goingup-glyph.png',
-        },
-        {
-            name: 'GoingUp',
-            description: 'Web3 Protocol for reputation & identity description',
-            address: '3',
-            logo: '/images/goingup-glyph.png',
-        },
-        {
-            name: 'GoingUp',
-            description: 'Web3 Protocol for reputation & identity description',
-            address: '4',
-            logo: '/images/goingup-glyph.png',
-        },
-        {
-            name: 'GoingUp',
-            description: 'Web3 Protocol for reputation & identity description',
-            address: '5',
-            logo: '/images/goingup-glyph.png',
-        },
-        {
-            name: 'GoingUp',
-            description: 'Web3 Protocol for reputation & identity description',
-            address: '6',
-            logo: '/images/goingup-glyph.png',
-        },
-    ];
+    const [loading, setLoading] = useState(false);
+    const [orgs, setOrgs] = useState([]);
 
-    const value = { organizations };
+    const logo = '/images/goingup-glyph.png';
+
+    const getOrgs = async () => {
+        const res = await fetch('/api/get-orgs');
+        const data = await res.json();
+        setOrgs(data);
+        setLoading(false);
+    };
+
+    const value = { logo, loading, orgs, setOrgs, setLoading, getOrgs };
 
     return (
         <OrganizationsContext.Provider value={value}>
