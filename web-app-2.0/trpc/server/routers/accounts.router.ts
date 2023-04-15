ef0f784b6b3c44c8deb57d359b412558af5f25eb
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-15 21:52:47 +0800

    Create account router

---
+++ web-app-2.0/trpc/server/routers/accounts.router.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { router, procedure } from '../trpc';
+import { getAccount } from '@/utils/database/account';
+
+export const accountsRouter = router({
+  get: procedure
+    .input(z.object({ address: z.string() }))
+    .query(async ({ input }) => {
+      return getAccount(input.address);
+    }),
+});
