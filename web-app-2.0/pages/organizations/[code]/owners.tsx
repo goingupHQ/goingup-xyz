cd76d139213ee3ce446cb4ea7ef8f36f493bc897
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-16 10:51:06 +0800

    Update owners.tsx
    Reformat

--- web-app-2.0/pages/organizations/[code]/owners.tsx
+++ web-app-2.0/pages/organizations/[code]/owners.tsx
@@ -1,9 +1,8 @@
 import { trpc } from '@/utils/trpc';
-import { Backdrop, Box, CircularProgress, Grid, Stack, Typography } from '@mui/material';
+import { Backdrop, CircularProgress, Grid } from '@mui/material';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Profile from '@/components/common/profile';
-import Navigation from '@/components/organizations/navigation';
 import OrgPageHeader from '@/components/organizations/org-page-header';
 
 export default function Owners() {
@@ -19,17 +18,33 @@ export default function Owners() {
 
       <OrgPageHeader org={organization} />
 
-      <Backdrop open={isLoading} sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ mt: 2 }}
+      >
+        {organization?.owners?.map((owner) => (
+          <Grid
+            item
+            xs={12}
+            md={6}
+            lg={4}
+            xl={3}
+          >
+            <Profile
+              key={owner}
+              addressOrAccount={owner}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
+      <Backdrop
+        open={isLoading}
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
         <CircularProgress color="inherit" />
       </Backdrop>
-
-      <Grid container spacing={2} sx={{ mt: 2 }}>
-      {organization?.owners?.map((owner) => (
-        <Grid item xs={12} md={6} lg={4} xl={3}>
-          <Profile key={owner} addressOrAccount={owner} />
-        </Grid>
-      ))}
-      </Grid>
     </>
   );
 }
