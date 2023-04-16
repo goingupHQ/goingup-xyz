b5a43e0e00276024ce20134d7764db515ff60afa
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-16 21:31:36 +0800

    Update create-reward-token.tsx

--- web-app-2.0/components/organizations/create-reward-token.tsx
+++ web-app-2.0/components/organizations/create-reward-token.tsx
@@ -1,7 +1,9 @@
-import { Organization } from "@/types/organization";
-import { Button } from "@mui/material";
-import { useSnackbar } from "notistack";
-import { useAccount } from "wagmi";
+import { Organization } from '@/types/organization';
+import { Button } from '@mui/material';
+import { useSnackbar } from 'notistack';
+import React from 'react';
+import { useAccount } from 'wagmi';
+import { NFTStorage } from 'nft.storage';
 
 type CreateRewardTokenProps = {
   org: Organization;
@@ -11,18 +13,49 @@ const CreateRewardToken = ({ org }: CreateRewardTokenProps) => {
   const { isConnected } = useAccount();
   const { enqueueSnackbar } = useSnackbar();
 
-  const handleCreateClick = () => {
-    if (!isConnected) {
-      enqueueSnackbar('Please connect your wallet first', { variant: 'error' });
-      return;
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+  const [tokenImageFile, setTokenImageFile] = React.useState<File | null>(null);
+
+  const handleCreateClick = async () => {
+    // if (!isConnected) {
+    //   enqueueSnackbar('Please connect your wallet first', { variant: 'error' });
+    //   return;
+    // }
+
+    if (tokenImageFile) {
+      console.log('tokenImageFile', tokenImageFile);
+      const client = new NFTStorage({ token: process.env.NEXT_PUBLIC_NFT_STORAGE_KEY });
+
+      const response = await client.store({
+        name: 'Test Name',
+        description: 'Test Description',
+        image: tokenImageFile
+      });
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setTokenImageFile(file);
     }
   };
 
   return (
     <>
-      <Button variant="contained" sx={{ my: 2 }}>Create a reward token</Button>
+      <input type="file" ref={fileInputRef} accept="image/*" onChange={handleFileChange} />
+
+      <Button
+        variant="contained"
+        sx={{ my: 2 }}
+        onClick={handleCreateClick}
+      >
+        Create a reward token
+      </Button>
+
+
     </>
-  )
-}
+  );
+};
 
 export default CreateRewardToken;
