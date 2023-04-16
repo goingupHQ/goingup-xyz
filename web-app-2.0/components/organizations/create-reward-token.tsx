7736c824b392c79c7efed3cba5c3f831abe6d386
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-16 18:14:18 +0800

    Partial work on creating reward tokens

--- web-app-2.0/components/organizations/create-reward-token.tsx
+++ web-app-2.0/components/organizations/create-reward-token.tsx
@@ -1,11 +1,23 @@
 import { Organization } from "@/types/organization";
 import { Button } from "@mui/material";
+import { useSnackbar } from "notistack";
+import { useAccount } from "wagmi";
 
 type CreateRewardTokenProps = {
   org: Organization;
 };
 
 const CreateRewardToken = ({ org }: CreateRewardTokenProps) => {
+  const { isConnected } = useAccount();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleCreateClick = () => {
+    if (!isConnected) {
+      enqueueSnackbar('Please connect your wallet first', { variant: 'error' });
+      return;
+    }
+  };
+
   return (
     <>
       <Button variant="contained" sx={{ my: 2 }}>Create a reward token</Button>
--- web-app-2.0/trpc/server/routers/organizations.router.ts
+++ web-app-2.0/trpc/server/routers/organizations.router.ts
@@ -4,21 +4,34 @@ import { createOrgCodes, get, getAll, isOwner } from '@/utils/database/organizat
 import admins from '@/utils/admins.json';
 import { validateSignature } from '@/utils/web3-signature';
 import { TRPCError } from '@trpc/server';
+import { ethers } from 'ethers';
+import { GoingUpUtilityTokens__factory } from '@/typechain';
+import { getMaxTokenId, getNextTokenId } from '@/utils/utility-tokens';
 
 export const organizationsRouter = router({
   getAll: procedure.query(async () => {
     const orgs = await getAll();
     return orgs;
   }),
-  get: procedure
-    .input(z.object({ code: z.string() }))
-    .query(async ({ input }) => {
-      return get(input.code);
-    }),
-  isOwner: procedure
-    .input(z.object({ code: z.string(), address: z.string() }))
-    .query(async ({ input }) => {
-      return isOwner(input.code, input.address);
+  get: procedure.input(z.object({ code: z.string() })).query(async ({ input }) => {
+    return get(input.code);
+  }),
+  createRewardToken: procedure
+    .input(z.object({ code: z.string(), address: z.string(), message: z.string(), signature: z.string() }))
+    .mutation(async ({ input }) => {
+      await checkOrgOwner(input);
+
+      const provider = new ethers.providers.AlchemyProvider(137, process.env.ALCHEMY_POLYGON_MAINNET);
+      const wallet = new ethers.Wallet(process.env.GOINGUP_BACKEND_PK, provider);
+
+      const utilityTokensContract = GoingUpUtilityTokens__factory.connect(
+        process.env.NEXT_PUBLIC_GOINGUP_UTILITY_TOKEN,
+        wallet
+      );
+
+      const tokenId = await getNextTokenId();
+
+
     }),
   createOrgCodes: procedure
     .input(
@@ -41,3 +54,15 @@ export const organizationsRouter = router({
       await createOrgCodes();
     }),
 });
+
+async function checkOrgOwner(input: { code: string; message: string; address: string; signature: string; }) {
+  const isSignatureValid = await validateSignature(input.address, input.message, input.signature);
+  if (!isSignatureValid) {
+    throw new TRPCError({ code: 'BAD_REQUEST', message: 'Invalid signature' });
+  }
+
+  if (!isOwner(input.code, input.address)) {
+    throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Not org owner' });
+  }
+}
+
