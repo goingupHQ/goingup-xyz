dee82fa27afcba204952b33c51c692be2dc21a18
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-16 16:07:52 +0800

    Update owners.tsx
    Add key to fix build error

--- web-app-2.0/pages/organizations/[code]/owners.tsx
+++ web-app-2.0/pages/organizations/[code]/owners.tsx
@@ -38,6 +38,7 @@ export default function Owners() {
             md={6}
             lg={4}
             xl={3}
+            key={owner}
           >
             <Profile
               key={owner}
