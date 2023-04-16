787ea1aa9d499b44dc5f1b84911d8c9b033e4514
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-16 21:38:31 +0800

    Update accounts.router.ts

--- web-app-2.0/trpc/server/routers/accounts.router.ts
+++ web-app-2.0/trpc/server/routers/accounts.router.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod';
-import { getAccount } from '@/utils/database/account';
+import { getAccount } from '../../../utils/database/account';
 import { router, procedure } from '../trpc';
 
 export const accountsRouter = router({
-  get: procedure
-    .input(z.object({ address: z.string() }))
-    .query(async ({ input }) => {
-      return getAccount(input.address);
-    }),
+  get: procedure.input(z.object({ address: z.string() })).query(async ({ input }) => {
+    return getAccount(input.address);
+  }),
 });
