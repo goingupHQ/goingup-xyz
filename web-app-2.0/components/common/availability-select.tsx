d306e99b4fb19730cbe60acda72ac3e7b906c9a7
Mark Ibanez <mark.ibanez@gmail.com>
2023-05-26 21:52:10 +0800

    Create availability-select.tsx
    This is a reusable searchable availability multi select component

---
+++ web-app-2.0/components/common/availability-select.tsx
@@ -0,0 +1,40 @@
+import { AppContext, Availability } from '@/contexts/app-context';
+import { Autocomplete, TextField } from '@mui/material';
+import { useContext, useState } from 'react';
+
+type AvailabilitySelectProps = {
+  value: Availability[];
+  setValue: (value: Availability[]) => void;
+  label?: string;
+};
+
+const AvailabilitySelect = ({ value, setValue, label }: AvailabilitySelectProps) => {
+  const app = useContext(AppContext);
+  const { availability } = app;
+  const [inputValue, setInputValue] = useState<string>('');
+
+  return (
+    <Autocomplete
+      multiple
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      inputValue={inputValue}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      options={availability}
+      getOptionLabel={(option: Availability) => option.text}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={label || 'Availability'}
+          required
+        />
+      )}
+    />
+  );
+};
+
+export default AvailabilitySelect;
