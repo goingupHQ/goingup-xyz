6499e1df99e734dc427b9e0b318dc575e84ed9ca
Mark Ibanez <mark.ibanez@gmail.com>
2023-10-22 21:02:12 +0800

    Remove unused imports

--- web-app-2.0/pages/organizations/index.js
+++ web-app-2.0/pages/organizations/index.js
@@ -1,11 +1,6 @@
+import { Typography } from '@mui/material';
 import Head from 'next/head';
-import { Typography, Box } from '@mui/material';
-import { AppContext } from '../../contexts/app-context';
-import { useContext, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { OrganizationsContext } from '../../contexts/organizations-context';
 import OrganizationsList from '../../components/pages/organizations/organizations-list';
-import LoadingIllustration from '../../components/common/loading-illustration';
 
 export default function Organizations() {
   return (
